f74fd3f1f59cca7d20299948f9fa30bd7dda98c3
waverDeep <dandyleader@naver.com>
2020-10-07 19:03:02 +0900

    Initial commit

---
+++ playground.py
@@ -0,0 +1,8 @@
+import os
+import glob
+
+
+# find all dataset filepath
+def get_all_file_path(input_dir, file_extension):
+    temp = glob.glob(os.path.join(input_dir, '**', '*.{}'.format(file_extension)), recursive=True)
+    return temp
